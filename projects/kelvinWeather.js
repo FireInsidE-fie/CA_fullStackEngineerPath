2412a4a7f60cb575aece815c5f356dfd64de4026
firefoxSpinnie <firefoxSpinnie@protonmail.ch>
2021-08-11 19:50:10 +0200

    Add ;s

--- projects/kelvinWeather.js
+++ projects/kelvinWeather.js
@@ -7,17 +7,17 @@ const kelvin = 0;
 const celsius = kelvin - 273;
 
 // Converts the celsius temparature to fahrenheit degrees (Fahrenheit = Celsius * (9/5) + 32).
-let fahrenheit = celsius * (9/5) + 32
+let fahrenheit = celsius * (9/5) + 32;
 
 // Rounds down the temperature in fahrenheit degrees.
-fahrenheit = Math.floor(fahrenheit)
+fahrenheit = Math.floor(fahrenheit);
 
 // Logs result to the console.
-console.log(`The temperature is ${fahrenheit} degress Fahrenheit.`)
+console.log(`The temperature is ${fahrenheit} degress Fahrenheit.`);
 
 // Converts to newton degrees and rounds it down.
-let newton = celsius * (33/100)
-newton = Math.floor(newton)
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
 
 // Logs the newton degrees to the console.
-console.log(`The temperature is ${newton} degrees Newton.`)
+console.log(`The temperature is ${newton} degrees Newton.`);
