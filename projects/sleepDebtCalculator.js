d6ccb44a2800335e7e09861bd8f2086ea3ca7ebf
firefoxSpinnie <firefoxSpinnie@protonmail.ch>
2021-08-18 14:19:19 +0200

    Add file for future project

---
+++ projects/sleepDebtCalculator.js
@@ -0,0 +1,2 @@
+// https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-part-i/modules/fecp-learn-javascript-syntax-functions/projects/sleep-debt-calculator
+
