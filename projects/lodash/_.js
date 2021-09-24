e03eeaa1e742a6b5a080096205241cbec5e3def7
firefoxSpinnie <firefoxSpinnie@protonmail.ch>
2021-09-24 09:42:01 +0200

    Add method .clamp() to lodash

--- projects/lodash/_.js
+++ projects/lodash/_.js
@@ -1,7 +1,26 @@
+let _ = {
+    /* Takes three number and returns the first number clamped to the two others,
+    or just the first number if it is between the two others.
+    */
+    clamp(number, lowest, highest) { 
+        if (number < highest && number > lowest) {
+            return number;
+        } else if (number > highest) {
+            return highest;
+        } else if (number < lowest) {
+            return lowest;
+        } else {
+            console.log('Error.')
+        }
+    },
 
-
-
-
+    /* Determines if a numnber is between the two others.
+    Will default start to 0 if end isn't specified, and take start as end.
+    */
+   inRange(number, start, end) {
+        
+   }
+};
 
 // Do not write or modify code below this line.
 module.exports = _;
