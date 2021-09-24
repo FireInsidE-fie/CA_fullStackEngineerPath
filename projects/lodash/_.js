d12e5c6065408c2437b808f2b928684a2eb4f397
firefoxSpinnie <firefoxSpinnie@protonmail.ch>
2021-09-24 19:10:37 +0200

    Add method .inRange() to lodash

--- projects/lodash/_.js
+++ projects/lodash/_.js
@@ -1,7 +1,6 @@
 let _ = {
     /* Takes three number and returns the first number clamped to the two others,
-    or just the first number if it is between the two others.
-    */
+    or just the first number if it is between the two others.*/
     clamp(number, lowest, highest) { 
         if (number < highest && number > lowest) {
             return number;
@@ -14,11 +13,31 @@ let _ = {
         }
     },
 
-    /* Determines if a numnber is between the two others.
-    Will default start to 0 if end isn't specified, and take start as end.
-    */
-   inRange(number, start, end) {
-        
+    /* Determines if a number is between the two others.
+    Will default start to 0 if end isn't specified, and take start as end.*/
+    inRange(number, start, end) {
+        if (end === undefined) {  // If the end scope isn't specified.
+            end = start;
+            start = 0;
+            if (number < end && number >= start) {  // If number is in range.
+                return true;
+            } else {  // If number isn't in range.
+                return false;
+            }
+        } else if (end < start) {  // If the start scope is higher than the end scope, will swap the two values.
+            [start, end] = [end, start];
+            if (number < end && number >= start) {  // If number is in range.
+                return true;
+            } else {  // If number isn't in range.
+                return false;
+            }
+        } else {
+            if (number < end && number >= start) {  // If number is in range.
+                return true;
+            } else {  // If number isn't in range.
+                return false;
+            }
+        }
    }
 };
 
