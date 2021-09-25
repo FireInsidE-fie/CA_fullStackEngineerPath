5f4fcb3990833f002cae85878559c539ac734b4d
firefoxSpinnie <firefoxSpinnie@protonmail.ch>
2021-09-25 16:56:12 +0200

    Add .pad() to lodash and fix a broken test

--- projects/lodash/_.js
+++ projects/lodash/_.js
@@ -1,6 +1,6 @@
 let _ = {
     /* Takes three number and returns the first number clamped to the two others,
-    or just the first number if it is between the two others.*/
+    or just the first number if it is between the two others. */
     clamp(number, lowest, highest) { 
         if (number < highest && number > lowest) {
             return number;
@@ -14,7 +14,7 @@ let _ = {
     },
 
     /* Determines if a number is between the two others.
-    Will default start to 0 if end isn't specified, and take start as end.*/
+    Will default start to 0 if end isn't specified, and take start as end. */
     inRange(number, start, end) {
         if (end === undefined) {  // If the end scope isn't specified.
             end = start;
@@ -40,8 +40,27 @@ let _ = {
         }
    },
 
-    words(inputString) {
-        return inputString.split(' ');
+    /* Splits a strings into words separated by a blank space. */
+    words(string) {
+        return string.split(' ');
+    },
+
+    /* Add empty spaces to each end of string if length is higher than string.length. */
+    pad(string, length) {
+        let stringArray = string.split('');
+        if (stringArray.length < length) {
+            for (let i = 0; i < string.length; i++) {
+                stringArray.push(' ');
+                stringArray.unshift(' ');
+            }
+
+            if ((string.length - length) % 2 != 0) {  // If the added blank spaces number is uneaven, add one at the end.
+                stringArray.push(' ')
+            }
+            return stringArray.join('');
+        } else {
+            return string;
+        }
     }
 };
 
