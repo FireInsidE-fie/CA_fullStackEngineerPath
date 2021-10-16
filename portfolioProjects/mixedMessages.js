cf17afb997397d654764dad8241746f6eec33280
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 10:21:22 +0000

    Add all messages part to build mixed messages

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -3,8 +3,10 @@
 // FireInsidE and Rykaqj
 
 // Creates the arrays that will contain the three parts of the return messages.
-let nouns = [];
-let verbs = [];
-let adjectives = [];
+let nouns = ["A Dog", "A Cat", "Your Computer", "A Human", "Your phone", "An insect", "My bed", "Your best friend", "A plushie", "A youtuber"];
+let verbs = ["runs", "swims", "eats", "sleeps", "drinks", "dancing", "skiing", "bowling"];
+let adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty", "brave", "foolish", "fancy", "macho", "tasty", "tricky",  ];
 
-
+const getRandomIndexOfArray = array => {
+  
+}
