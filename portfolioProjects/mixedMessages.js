9b3921392d73042fe59b0b356a9b5f4965f2b8a5
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 11:14:05 +0000

    Minor change to mixedMessages

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -8,7 +8,7 @@ const verbs = ["run", "swim", "eat", "sleep", "drink", "dance", "ski", "bowl", "
 const adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty", "brave", "foolish", "fancy", "macho", "tasty", "tricky", "slimy" ];
 
 // Function that will generate a random index of a given array.
-let getRandomIndexOfArray = array => { return Math.round(Math.random() * (array.length - 1)); }
+let getRandomIndexOfArray = array => { return Math.floor(Math.random() * array.length); }
 
 // Returns the end message.
 console.log(`I'm walking with my ${nouns[getRandomIndexOfArray(nouns)]}, and I encounter a(n) ${adjectives[getRandomIndexOfArray(adjectives)]} ${nouns[getRandomIndexOfArray(nouns)]}. I ${verbs[getRandomIndexOfArray(verbs)]} with them, but my ${nouns[getRandomIndexOfArray(nouns)]} is becoming ${adjectives[getRandomIndexOfArray(adjectives)]}, so I ${verbs[getRandomIndexOfArray(verbs)]} to the ${nouns[getRandomIndexOfArray(nouns)]}.`);
