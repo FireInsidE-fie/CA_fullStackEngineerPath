74e3e40015b6dccaa7ba6b9e2329aff28e2738b9
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 10:50:59 +0000

    Revamp messages generating and end message

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -3,11 +3,16 @@
 // FireInsidE and Rykaqj
 
 // Creates the arrays that will contain the three parts of the return messages.
-let nouns = ["Dog", "Cat", "Computer", "Human", "Phone", "insect", "bed", "best friend", "Plushie", "Youtuber"];
-let verbs = ["runs", "swims", "eats", "sleeps", "drinks", "dance", "ski", "bowl", "drive"];
-let adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty", "brave", "foolish", "fancy", "macho", "tasty", "tricky", "slimy" ];
+const nouns = ["Dog", "Cat", "Computer", "Human", "Phone", "insect", "bed", "best friend", "Plushie", "Youtuber"];
+const verbs = ["runs", "swims", "eats", "sleeps", "drinks", "dance", "ski", "bowl", "drive"];
+const adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty", "brave", "foolish", "fancy", "macho", "tasty", "tricky", "slimy" ];
 
 // Function that will generate a random index of a given array.
-const getRandomIndexOfArray = array => { return Math.round(Math.random() * array.length); }
+let getRandomIndexOfArray = array => { return Math.round(Math.random() * array.length); }
 
-console.log(`As I was walking with my ${nouns[getRandomIndexOfArray(nouns)]}, I encountered a ${adjectives[getRandomIndexOfArray(adjectives)]} ${nouns[getRandomIndexOfArray(nouns)]}.`)
+// Creates randomly generated words to complete the end message.
+const randomNouns = nouns[getRandomIndexOfArray(nouns)];
+const randomVerbs = verbs[getRandomIndexOfArray(verbs)];
+const randomAdjective = adjectives[getRandomIndexOfArray(adjectives)];
+
+console.log(`I'm walking with my ${randomNoun}, and I encounter a(n) ${randomAdj} ${randomNoun}. I ${randomVerb} with them, but my ${randomNoun} is becoming ${randomAdj}, so I ${randomVerb} again.`);
