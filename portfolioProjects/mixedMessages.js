5681e9156ea03254a7689fbe86dd6b3f25a31c2d
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 11:30:58 +0000

    Enhance returned message

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -2,7 +2,7 @@
 // 16.10.2021
 // FireInsidE and Rykaqj
 
-// Creates the arrays that will contain the three parts of the return messages.
+// Creates the arrays that will contain all the parts of the return messages.
 const nouns = ["Dog", "Cat", "Computer", "Human", "Phone", "insect", "bed", "best friend", "Plushie", "Youtuber"];
 const verbs = ["run", "swim", "eat", "sleep", "drink", "dance", "ski", "bowl", "drive"];
 const adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty", "brave", "foolish", "fancy", "macho", "tasty", "tricky", "slimy" ];
@@ -10,5 +10,5 @@ const adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty"
 // Function that will generate a random index of a given array.
 let getRandomIndexOfArray = array => { return Math.floor(Math.random() * array.length); }
 
-// Returns the end message.
-console.log(`I'm walking with my ${nouns[getRandomIndexOfArray(nouns)]}, and I encounter a(n) ${adjectives[getRandomIndexOfArray(adjectives)]} ${nouns[getRandomIndexOfArray(nouns)]}. I ${verbs[getRandomIndexOfArray(verbs)]} with them, but my ${nouns[getRandomIndexOfArray(nouns)]} is becoming ${adjectives[getRandomIndexOfArray(adjectives)]}, so I ${verbs[getRandomIndexOfArray(verbs)]} to the ${nouns[getRandomIndexOfArray(nouns)]}.`);
+// Logs the end message to the console.
+console.log(`I'm walking with my ${nouns[getRandomIndexOfArray(nouns)]}, and I encounter a(n) ${adjectives[getRandomIndexOfArray(adjectives)]} ${nouns[getRandomIndexOfArray(nouns)]}. I ${verbs[getRandomIndexOfArray(verbs)]} with them, but my ${nouns[getRandomIndexOfArray(nouns)]} is becoming ${adjectives[getRandomIndexOfArray(adjectives)]}, so I ${verbs[getRandomIndexOfArray(verbs)]} to the ${nouns[getRandomIndexOfArray(nouns)]}.  We ${verbs[getRandomIndexOfArray(verbs)]} into the ${adjectives[getRandomIndexOfArray(adjectives)]} ${nouns[getRandomIndexOfArray(nouns)]} and the ${nouns[getRandomIndexOfArray(nouns)]} ${verbs[getRandomIndexOfArray(verbs)]} us all the way home.`);
