7c1aadbdeef468f704ee2bfc12134a5850e8fe95
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 10:09:59 +0000

    Add jokes in mixedMessages and replit config

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -1,4 +1,17 @@
 // https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages
 // 16.10.2021
-// FireInsidE and quang
+// FireInsidE and Rykaqj
 
+// Creates the array that will contain all bad jokes that will get logged to the console by the script.
+let messages = [
+  "Imagine if the U.S. switched from pounds to kilograms overnight... it would be mass confusion.",
+  "I was so bored today that I memorized five pages out of a dictionary. I learned next to nothing.",
+  "How do you make a water bed more bouncy? You fill it with spring water.",
+  "How do trees access the internet? They log in.",
+  "I decided to kill off some characters in the book I am writing... It would definitely spice up my autobiography.",
+  "Did you hear the joke about the toilet? Nevermind, it’s too dirty.",
+  "Why are elevator jokes the best jokes? Because they work on so many levels.",
+  "What do you call a pretty ghost? BOOtiful.",
+  "I finally quit drinking for good. Now I drink for evil.",
+  "How do you keep a bull from charging? By canceling its credit card."
+]
