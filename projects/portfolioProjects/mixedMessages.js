fff244a2e6f6d05da4f3fde142103ea168a57a34
FireInsidE <firefoxspinnie@protonmail.ch>
2021-10-16 11:13:54 +0200

    Add start of mixed messages and reorganise files

---
+++ projects/portfolioProjects/mixedMessages.js
@@ -0,0 +1,4 @@
+// https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages
+// 16.10.2021
+// FireInsidE and quang
+
