05f1bd188ebd84674f78585b50edf9f79813ebcc
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 10:33:19 +0000

    Add random index function and beginning of message

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -3,10 +3,11 @@
 // FireInsidE and Rykaqj
 
 // Creates the arrays that will contain the three parts of the return messages.
-let nouns = ["A Dog", "A Cat", "Your Computer", "A Human", "Your phone", "An insect", "My bed", "Your best friend", "A plushie", "A youtuber"];
-let verbs = ["runs", "swims", "eats", "sleeps", "drinks", "dancing", "skiing", "bowling"];
-let adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty", "brave", "foolish", "fancy", "macho", "tasty", "tricky",  ];
+let nouns = ["Dog", "Cat", "Computer", "Human", "Phone", "insect", "bed", "best friend", "Plushie", "Youtuber"];
+let verbs = ["runs", "swims", "eats", "sleeps", "drinks", "dance", "ski", "bowl", "drive"];
+let adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty", "brave", "foolish", "fancy", "macho", "tasty", "tricky", "slimy" ];
 
-const getRandomIndexOfArray = array => {
-  
-}
+// Function that will generate a random index of a given array.
+const getRandomIndexOfArray = array => { return Math.round(Math.random() * array.length); }
+
+console.log(`As I was walking with my ${nouns[getRandomIndexOfArray(nouns)]}, I encountered a ${adjectives[getRandomIndexOfArray(adjectives)]} ${nouns[getRandomIndexOfArray(nouns)]}.`)
