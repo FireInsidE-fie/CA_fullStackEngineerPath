0a129f1736fc7895ba4d696ede442d512faa7f65
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 10:53:07 +0000

    Fix end message

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -11,8 +11,8 @@ const adjectives = ["adorable", "evil", "deadly", "charming", "average", "salty"
 let getRandomIndexOfArray = array => { return Math.round(Math.random() * array.length); }
 
 // Creates randomly generated words to complete the end message.
-const randomNouns = nouns[getRandomIndexOfArray(nouns)];
-const randomVerbs = verbs[getRandomIndexOfArray(verbs)];
-const randomAdjective = adjectives[getRandomIndexOfArray(adjectives)];
+const randomNoun = nouns[getRandomIndexOfArray(nouns)];
+const randomVerb = verbs[getRandomIndexOfArray(verbs)];
+const randomAdj = adjectives[getRandomIndexOfArray(adjectives)];
 
 console.log(`I'm walking with my ${randomNoun}, and I encounter a(n) ${randomAdj} ${randomNoun}. I ${randomVerb} with them, but my ${randomNoun} is becoming ${randomAdj}, so I ${randomVerb} again.`);
