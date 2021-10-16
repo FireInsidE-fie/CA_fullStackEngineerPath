2af68fb32a6f64921f61f0f214eb6052f94bf7d2
FireInsidE <87356603+FireInsidE-fie@users.noreply.github.com>
2021-10-16 10:14:07 +0000

    Delete old jokes array and add three new ones

--- portfolioProjects/mixedMessages.js
+++ portfolioProjects/mixedMessages.js
@@ -2,16 +2,9 @@
 // 16.10.2021
 // FireInsidE and Rykaqj
 
-// Creates the array that will contain all bad jokes that will get logged to the console by the script.
-let messages = [
-  "Imagine if the U.S. switched from pounds to kilograms overnight... it would be mass confusion.",
-  "I was so bored today that I memorized five pages out of a dictionary. I learned next to nothing.",
-  "How do you make a water bed more bouncy? You fill it with spring water.",
-  "How do trees access the internet? They log in.",
-  "I decided to kill off some characters in the book I am writing... It would definitely spice up my autobiography.",
-  "Did you hear the joke about the toilet? Nevermind, it’s too dirty.",
-  "Why are elevator jokes the best jokes? Because they work on so many levels.",
-  "What do you call a pretty ghost? BOOtiful.",
-  "I finally quit drinking for good. Now I drink for evil.",
-  "How do you keep a bull from charging? By canceling its credit card."
-]
+// Creates the arrays that will contain the three parts of the return messages.
+let nouns = [];
+let verbs = [];
+let adjectives = [];
+
+
