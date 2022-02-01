742cfd0d1c6d918e170f727d072bf6ce46fa90ae
FireInsidE <firefoxspinnie@protonmail.ch>
2022-02-01 17:47:53 +0100

    BOOM

--- javaScript/find-your-hat-starting/main.js
+++ javaScript/find-your-hat-starting/main.js
@@ -5,5 +5,42 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
-const name = prompt('What is your name? ');
-console.log(`Welcome ${name}!`)
+// Prompting tests
+//const name = prompt('What is your name? ');
+//console.log(`Welcome ${name}!`)
+
+// Field class
+class Field {
+  constructor(field) {
+    this.field = field;
+    this.player = {
+      gameOn : true,
+      positionX : 0,
+      positionY : 0
+    };
+  }
+
+  print() {
+    for (let i = 0; i < this.field.length; i++) {
+      console.log(this.field[i].join(''));
+    }
+  }
+}
+
+// Creating field
+const playerField = new Field([
+  ['*', '░', '░', 'O', 'O', '░'],
+  ['O', 'O', '░', '░', '░', 'O'],
+  ['^', '░', 'O', '░', 'O', 'O'],
+  ['░', '░', '░', '░', 'O', '░']
+]);
+
+// Initializing player movement
+while (playerField.gameOn = true) {
+  playerField.print();
+  let direction = prompt('Where will you go? (wasd)');
+
+  if (direction = 'd') {
+
+  }
+}
